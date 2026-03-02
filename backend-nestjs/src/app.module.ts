76eb4ee54e31956881b1b4257528f512b0dca513
vishalmishradev <vishalmishra@medianv.com>
2026-03-02 17:43:38 +0530

    removed redundent codes

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -5,32 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthUser, User } from './entities';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
-import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     CrudModule,
     AuthModule,
-    ChatModule,
     DatabaseModule.forRoot({
       dbName: 'user_management_demo',
       entities: [AuthUser, User],
     }),
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: (configService: ConfigService) => ({
-    //     type: 'postgres',
-    //     host: configService.get('DB_HOST'),
-    //     port: configService.get('DB_PORT'),
-    //     username: configService.get('DB_USER'),
-    //     password: configService.get('DB_PASSWORD'),
-    //     database: configService.get('DB_NAME'),
-    //     entities: [AuthUser, User],
-    //     synchronize: true,
-    //   }),
-    // }),
   ],
 })
 export class AppModule {}
